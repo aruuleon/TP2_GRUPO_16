c816778685384a795125e0ade406814258cc7287
diazemilianoariel <emiliano.diaz@alumnos.frgp.utn.edu.ar>
2025-04-05 16:12:41 -0300

    clase EdificioOficina

---
+++ src/ejercicio3/EdificioOficina.java
@@ -0,0 +1,25 @@
+package ejercicio3;
+
+public class EdificioOficina {
+	
+	
+	// atributos
+	private int numeroDeOficinas;
+
+	
+	
+	// constructor 
+	public EdificioOficina(int numeroDeOficinas) {
+		this.numeroDeOficinas = numeroDeOficinas;
+	}
+	
+	
+	
+
+	
+	
+	
+
+	
+
+}
