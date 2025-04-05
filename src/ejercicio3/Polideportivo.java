f0900b2a435b995e4c323c7147be28a00e4e3683
MayraGonzalezN <mayragonzalezdd@gmail.com>
2025-04-05 17:29:48 -0300

    ejercicio 3 implementacion de la interfaz

--- src/ejercicio3/Polideportivo.java
+++ src/ejercicio3/Polideportivo.java
@@ -1,18 +1,21 @@
 package ejercicio3;
 
-public class Polideportivo {
+public class Polideportivo implements IInstalacionDeportiva {
 	
 	private String nombre;
     private double superficie;
+    private int tipoDeInstalacion;
     
     public Polideportivo() {
         setNombre("sin nombre");
         setSuperficie(0);
+        setTipoDeInstalacion(0);
     }
     
-    public Polideportivo(String nombre, double superficie) {
+    public Polideportivo(String nombre, double superficie, int tipoDeInstalacion) {
     	setNombre(nombre);
         setSuperficie(superficie);
+        setTipoDeInstalacion(tipoDeInstalacion);
     }
     
     public String getNombre() {
@@ -26,5 +29,13 @@ public class Polideportivo {
     public void setSuperficie(double superficie) {
     	this.superficie = superficie;
     }
+    
+    public void setTipoDeInstalacion(int tipoDeInstalacion) {
+	   this.tipoDeInstalacion = tipoDeInstalacion;
+   }
 
+	@Override
+	public int getTipoDeInstalacion() {
+		return tipoDeInstalacion;
+	}
 }
