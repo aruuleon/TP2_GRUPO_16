fd5d50643d745637fd854c91bf43e2207bcb520a
tamicaa-mat <tamicaa.mat@gmail.com>
2025-04-05 21:59:41 -0300

    implementacion IEdificio en EdificioOficina

--- src/ejercicio3/EdificioOficina.java
+++ src/ejercicio3/EdificioOficina.java
@@ -1,19 +1,57 @@
 package ejercicio3;
 
-public class EdificioOficina {
+public class EdificioOficina implements IEdificio{
 	
 	
 	// atributos
 	private int numeroDeOficinas;
-
+	private double superficieEdificioOficina;
 	
 	
 	// constructor 
 	public EdificioOficina(int numeroDeOficinas) {
 		this.numeroDeOficinas = numeroDeOficinas;
 	}
+
+
+	public int getNumeroDeOficinas() {
+		return numeroDeOficinas;
+	}
+
+
+	public void setNumeroDeOficinas(int numeroDeOficinas) {
+		this.numeroDeOficinas = numeroDeOficinas;
+	}
+
+
+	public double getSuperficieEdificioOficina() {
+		return superficieEdificioOficina;
+	}
+
+
+	public void setSuperficieEdificioOficina(double superficieEdificioOficina) {
+		this.superficieEdificioOficina = superficieEdificioOficina;
+	}
+
+
+	@Override
+	public String toString() {
+		return "EdificioOficina [numeroDeOficinas=" + numeroDeOficinas + ", superficieEdificioOficina="
+				+ superficieEdificioOficina + "]";
+	}
+
+
+	@Override
+	public double getSuperficieEdificio() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
 	
+
 	
+
 	
 
 	
